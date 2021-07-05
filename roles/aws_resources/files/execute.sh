f3d1d1967120541f0f260fc24ed68a8209233689
Venkata Nagendra Dittakavi <nvdittakavi@gmail.com>
2021-07-05 09:36:58 +0530

    Updated Ansible Scripts

--- roles/aws_resources/files/execute.sh
+++ roles/aws_resources/files/execute.sh
@@ -4,11 +4,6 @@
 # Shell Script to check for all the resources across all the regoins on AWS
 # Executes on RedHat or its Linux Flavours
 
-#Script to install and configure Unzip Wget
-sudo yum update -y
-sudo yum install wget -y
-sudo yum install zip unzip -y
-
 #Install Ansible and Configure
 sudo yum  install  https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 sudo yum update -y
@@ -17,18 +12,19 @@ sudo yum install ansible -y
 ansible --version
 
 #Install Ansible and Configure for RHEL 8
-sudo dnf update
-sudo dnf install python3
-sudo dnf install python3-pip
-pip3 install ansible --user
+#sudo dnf update
+#sudo dnf install python3
+#sudo dnf install python3-pip
+#pip3 install ansible --user
 ansible --version
-
+#
 # Install Dependencies
-sudo yum install pip
-pip install aws-list-all
+sudo yum install pip -y
+sudo yum install git -y
+sudo pip install aws-list-all
 
 # Clone the Git Repository
-https://github.com/nvdittakavi/aws_list_all.git
+# git clone https://github.com/nvdittakavi/aws_list_all.git
 
 # Edit the Region and Services File
 ansible-vault edit resources.vault
